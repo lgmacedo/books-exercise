a415b5134c1beeec6d8947c57cf06cf546e76f3e
lgmacedo <lucasguerra.macedo@gmail.com>
2023-07-05 20:27:44 -0300

    code refactored for prisma

--- src/protocols/book.ts
+++ src/protocols/book.ts
@@ -1,12 +1,5 @@
-export type Book = {
-  id: number;
-  title: string;
-  author: string;
-  publisher: string;
-  purchaseDate: Date;
-  read: boolean;
-  review: string;
-  grade: number;
-};
+import { Book as BookDB } from "@prisma/client";
+
+export type Book = BookDB;
 
 export type CreateBook = Omit<Book, "id" | "read" | "review" | "grade">;
